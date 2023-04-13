780dd8669c083b362a24cf89be1a0d9ef6498c88
Victoria de los Ángeles Solís Pérez <dual.victoria.solis@gmail.com>
2023-04-13 15:25:00 -0600

    Creación de clases para el modelo.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Poliza (models.Model):
+    Num_Poliza = models.IntegerField()
+    Fecha_Inicio = models.DateField()
+    Fecha_Vigencia = models.DateField()
+    Id_Cliente = models.ForeignKey (Cliente, on_delete=models.CASCADE)
+    Id_Asegurado = models.ForeignKey(Asegurado, on_delete=models.CASCADE)
+    Aseguradora = models.CharField(max_length=200)
+    Tipo_Poliza = models.ForeignKey(Tipo_Poliza, on_delete=models.CASCADE)
+    Precio = models.DecimalField()
+    Estado_Poliza = models.ForeignKey(Estado_Poliza, on_delete=models.CASCADE)
+
+class Cliente (models.Model):
+    Nombre_Cliente = models.CharField(max_length=200)
+    Telefono = models.CharField(max_length=10)
+    Email = models.CharField(max_length=50)
+    Id_Agente = models.ForeignKey(Agente, on_delete=models.CASCADE)
+
+class Agente (models.Model):
+    Nombre_Agente = models.CharField(max_length=200)
+    Email = models.CharField(max_length=50)
+    Password = models.CharField(max_length=50)
+
+class Asegurado (models.Model):
+    Nombre_Asegurado = models.CharField(max_length=200)
+    
+class Tipo_Poliza (models.Model):
+    Tipo_Poliza = models.CharField(max_length=200)
+
+class Estado_Poliza (models.Model):
+    Estado_Poliza = models.CharField(max_length=50)
