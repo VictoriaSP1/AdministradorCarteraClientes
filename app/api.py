3010c601e1ea5f024c372cf0c5b6bf94165171ae
Victoria de los Ángeles Solís Pérez <dual.victoria.solis@gmail.com>
2023-04-14 11:01:55 -0600

    Serializers y configuraciones adicionales

---
+++ app/api.py
@@ -0,0 +1,9 @@
+from .models import Agente
+from rest_framework import viewsets, permissions
+from .serializers import AgenteSerializer
+
+class AgenteViewSet(viewsets.ModelViewSet):
+    queryset = Agente.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = AppSerializer
+
